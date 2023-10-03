9b30a080c3687442d2df32d2091a86f581d5a6d9
AnaTMorales <moralesgarciaanateresa@gmail.com>
2023-10-03 16:48:02 -0500

    Notificaciones

--- notification.py
+++ notification.py
@@ -1,2 +1,6 @@
-def notificar_ciudadano(reporte, mensaje):
-    reporte.ciudadano.recibir_notificacion(mensaje)
+class Notificaciones:
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+
+    def notificar_ciudadano(reporte, mensaje):
+        reporte.ciudadano.recibir_notificacion(mensaje)

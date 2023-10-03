25701fe6daf0dcd4b93d6fe8e5e3ba72a1c3e556
AnaTMorales <130396209+AnaTMorales@users.noreply.github.com>
2023-10-03 13:34:12 -0500

    Update location.py

--- location.py
+++ location.py
@@ -1,11 +1,11 @@
 class Location:
-    def __init__(self, id_ubicacion,coordenadas, nombre_lugar):
+    def __init__(self, id_ubicacion, coordenadas, nombre_lugar):
         self.id_ubicacion = id_ubicacion
         self.coordenadas = coordenadas
         self.nombre_lugar = nombre_lugar
 
     def mostrar_ubicacion(self):
-        print("Nombre del lugar: {self.nombre_lugar}")
-        print("Coordenadas de la ubicación: {self.coordenadas}")
+        print(f"Nombre del lugar: {self.nombre_lugar}")
+        print(f"Coordenadas de la ubicación: {self.coordenadas}")
 
 

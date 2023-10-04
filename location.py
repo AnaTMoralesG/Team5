4b6223128b6527b03623c22d5a379af3a608ef7b
CarlosChan013 <106627780+CarlosChan013@users.noreply.github.com>
2023-10-04 09:11:27 -0500

    segundo commit

---
+++ location.py
@@ -0,0 +1,9 @@
+class Location:
+    def __init__(self, id_ubicacion, coordenadas, nombre_lugar):
+        self.id_ubicacion = id_ubicacion
+        self.coordenadas = coordenadas
+        self.nombre_lugar = nombre_lugar
+
+    def mostrar_ubicacion(self):
+        print(f"Nombre del lugar: {self.nombre_lugar}")
+        print(f"Coordenadas de la ubicación: {self.coordenadas}")

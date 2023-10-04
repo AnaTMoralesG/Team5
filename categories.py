ffd385d11a4f6172ed2363f8cfc74d114a65b134
AnaTMorales <moralesgarciaanateresa@gmail.com>
2023-10-03 19:42:55 -0500

    Categorias

--- categories.py
+++ categories.py
@@ -1,44 +1,52 @@
-def mostrar_categorias():
-    print("1. Categoría 1")
-    print("2. Categoría 2")
-    print("3. Categoría 3")
-    print("4. Categoría 4")
+class Categorias:
+    def _init_(self):
+        self.opcion = 0
 
-def categoria_1():
-    print("Bienvenido a la Categoría 1")
-    # Lógica y funcionalidades para la Categoría 1
+    def mostrar_categorias(self):
+        print("1. Categoría 1")
+        print("2. Categoría 2")
+        print("3. Categoría 3")
+        print("4. Categoría 4")
 
-def categoria_2():
-    print("Bienvenido a la Categoría 2")
-    # Lógica y funcionalidades para la Categoría 2
+    def categoria_1(self):
+        print("Bienvenido a la Categoría 1")
+        # Lógica y funcionalidades para la Categoría 1
 
-def categoria_3():
-    print("Bienvenido a la Categoría 3")
-    # Lógica y funcionalidades para la Categoría 3
+    def categoria_2(self):
+        print("Bienvenido a la Categoría 2")
+        # Lógica y funcionalidades para la Categoría 2
 
-def categoria_4():
-    print("Bienvenido a la Categoría 4")
-    # Lógica y funcionalidades para la Categoría 4
+    def categoria_3(self):
+        print("Bienvenido a la Categoría 3")
+        # Lógica y funcionalidades para la Categoría 3
 
-def seleccionar_categoria():
-    while True:
-        try:
-            opcion = int(input("Selecciona una categoría (1-4) o 0 para salir: "))
-            if opcion == 1:
-                categoria_1()
-            elif opcion == 2:
-                categoria_2()
-            elif opcion == 3:
-                categoria_3()
-            elif opcion == 4:
-                categoria_4()
-            elif opcion == 0:
-                print("Hasta luego. ¡Adiós!")
-                break
-            else:
-                print("Por favor, selecciona una opción válida (1-4) o 0 para salir.")
-        except ValueError:
-            print("Por favor, ingresa un número válido.")
+    def categoria_4(self):
+        print("Bienvenido a la Categoría 4")
+        # Lógica y funcionalidades para la Categoría 4
 
-mostrar_categorias()
-seleccionar_categoria()
+    def seleccionar_categoria(self):
+        while True:
+            try:
+                self.mostrar_categorias()
+                self.opcion = int(input("Selecciona una categoría (1-4) o 0 para salir: "))
+                if self.opcion == 1:
+                    self.categoria_1()
+                elif self.opcion == 2:
+                    self.categoria_2()
+                elif self.opcion == 3:
+                    self.categoria_3()
+                elif self.opcion == 4:
+                    self.categoria_4()
+                elif self.opcion == 0:
+                    print("Hasta luego. ¡Adiós!")
+                    break
+                else:
+                    print("Por favor, selecciona una opción válida (1-4) o 0 para salir.")
+            except ValueError:
+                print("Por favor, ingresa un número válido.")
+
+# Crear una instancia de la clase Categorias
+categorias = Categorias()
+
+# Llamar al método seleccionar_categoria de la instancia
+categorias.seleccionar_categoria()
